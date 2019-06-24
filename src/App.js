5dc79e0e52c23c9a80dd78e753fc4ab98e5fed6b
mayur <mayur.pehlwani@zemosolabs.com>
2019-06-24 13:59:30 +0530

    adding items to list

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const List=(props)=>{
+const list= props.items.map((value,index)=><li key={index}>{value}</li>);
+  return(
+  <ul>
+   {list}
+  </ul>
+);
 }
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.ADD=this.ADD.bind(this);
+    this.state = {
+      val: '',
+      items: []
+    }
+  }
+  handleChange(e) {
+    let val = e.target.value;
+    this.setState({val});
+  }
+  ADD(){
+    const previtems=this.state.items;
+    this.setState({
+      items:[...previtems,this.state.val],
+      val:''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <input value={this.state.val} onChange={this.handleChange}/>
+        <button onClick={this.ADD}>ADD</button>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+}
 export default App;
